3d032f4f1aae31a8d442e89799842493b37d8a67
Mintu Sikder <mintusikder15@gmail.com>
2025-05-31 14:03:14 +0600

    added card details page

--- client/src/components/CoffeeDetails.jsx
+++ client/src/components/CoffeeDetails.jsx
@@ -1,13 +1,35 @@
-import React from 'react';
-import { useLoaderData } from 'react-router';
+import React from "react";
+import { useLoaderData } from "react-router";
 
 const CoffeeDetails = () => {
-  const {data:coffee} = useLoaderData()
-  const {email,photo,details,price,taste,supplier,quantity,name,_id} = coffee || {}
-  console.log(coffee.data)
+  const { data: coffee } = useLoaderData();
+  const {
+    email,
+    photo,
+    details,
+    price,
+    taste,
+    supplier,
+    quantity,
+    name,
+    _id,
+    likedBy,
+  } = coffee || {};
+  console.log(coffee.data);
   return (
-    <div>
+    <div className="flex gap-8 justify-center items-center py-12">
+      <div className="flex-1">
         <img src={photo} alt="" />
+      </div>
+      <div className="flex-1">
+        <p className="text-3xl font-bold">Details: {details}</p>
+        <p className="text-3xl ">Price: {price}</p>
+        <p>Likes: {likedBy.length}</p>
+        <div className="flex gap-3 mt-3">
+          <button className="btn btn-primary">Order</button>
+          <button className="btn btn-secondary">Like</button>
+        </div>
+      </div>
     </div>
   );
 };
